9103a98fcfae1eb0c308d3fc4ca1493c12b24656
Denmfsj <mfsj.p2@gmail.com>
2025-04-11 18:45:16 +0200

    adding file from previous lesson

---
+++ main.py
@@ -0,0 +1,54 @@
+
+
+MAX_AGE = 99  # constant
+
+
+# - комментар
+my_name = 'Denys'  # string, str
+my_age = 33 # integer, int
+my_temp = 36.6  # float, число з точкою
+my_friends = ['Alex', 'Ivan']  # list, список
+my_info = {'name': 'denys', 'age': 33}  # dict, dictionary, слщвник
+my_skills = {'aq', 'aqa'}  # set, колекція
+my_cats = ('Aria', 'Finia')  # tuple
+am_i_exist = True  # boolean
+None # None, нічого
+
+# print(my_name)
+
+my_sum = my_age + my_temp  #  нова змінна my_sum, float
+my_min = my_age - my_temp  #  нова змінна my_min, float
+my_mul = my_age * my_temp  #  нова змінна my_mul, float
+my_div = my_age / my_temp  #  нова змінна my_div, float
+
+# print(my_sum)
+# print(my_min)  # -3.0000000000000014
+# print(my_mul)
+# print(my_div)
+
+# print(int(-3.6) == int(my_min))  # int(-3.6) == -3, int(my_min) = int(-3.0000000000000014) = -3
+# print(-3.6,  int(my_min))
+# # rounded_value = round(my_min, 1)  # round(що округлювати,  скільки чисел після точки)
+# # print(rounded_value)
+#
+# # print(33 + 33)
+# # print(44 - 33)
+# # print( 55 * 5)
+# print( 10 / 2)
+# print(10.2)
+
+apples = 4
+peer = apples + 5
+var_3 = peer - 2
+
+print("Було 'Apple OOO' ", apples, 'яблук, та', peer, '''груш, та''', peer - 2, "слив")
+
+
+if my_age == 33: # якщо в змінній my_age 'лежить' число 33 то друкуй що нижче
+    print('you are 33 years old')
+
+
+print('Done')
+
+name_1_asd = 'asd'
+
